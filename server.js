45e43e9b0f0ff2fd9ff4d9cc1a9353f7b806e527
Sachin <vs1sachi12@gmail.com>
2025-04-10 16:04:10 +0530

    change password

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require("express")
+const cors = require("cors")
 
-app.get('/',(req,res)=>{
-    res.json({
-        mesg: 'Hello Sachin'
-    })
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors({
+    origin : "*",
+    methods : ["GET","POST","PUT","PATCH","DELETE"]
+}))
+
+app.get("/",(req,res)=>{
+    res.status(200).json({success:true,message:"Hello World 13"})
 })
 
-app.listen(5000)
+app.listen(5000,()=>{
+    console.log("Listening on port 5000");
+})
